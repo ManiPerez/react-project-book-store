03bd3c03e0b09e973a185cbb61b989312b4ab1f5
Magdalena <perezmagdalenaines@gmail.com>
2022-01-14 08:08:09 -0300

    file changes

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { useState, useEffect } from 'react'
-import ItemDetail from '../ItemDetail/ItemDetail'
-import './styles.css'
-import { getProductById } from '../../products'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+import ItemDetail from '../ItemDetail/ItemDetail';
+import { getProductById } from '../../products';
+import { useParams } from 'react-router-dom';
 
-const ItemDetailContainer = ()=> {
+const ItemDetailContainer = () => {
     const [product, setProduct] = useState()
     const [inputType, setInputType] = useState('input')
 
@@ -24,15 +23,13 @@ const ItemDetailContainer = ()=> {
 
     }, [paramId])
 
-
     return (
         <div className="ItemDetailContainer" >
             <button onClick={() => setInputType('input')}>Input</button>
             <button onClick={() => setInputType('button')}>Button</button>
             <ItemDetail  product={product} inputType={inputType}/>
         </div>
-    )    
-    
+    )       
 }
 
 export default ItemDetailContainer
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import ItemList from '../ItemList/ItemList';
 import { getProducts } from '../../products';
 import { useParams } from 'react-router-dom';
 
-const ItemListContainer = ()=> {
+const ItemListContainer = () => {
     const [products, setProducts] = useState([])
     const { categoryId } = useParams()
 
