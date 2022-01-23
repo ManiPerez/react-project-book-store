ec0621fbb49297c0805cbcfa5c9e0b9b536edb7b
Magdalena <perezmagdalenaines@gmail.com>
2022-01-23 16:54:23 -0300

    file changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './Context/CartContext';
 import NavBar from './Components/Navbar/Navbar';
 import Cart from './Components/Cart/Cart'
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
@@ -11,18 +10,16 @@ const App = () => {
 
   return (
     <>
-      <CartProvider>
-          <BrowserRouter>
-            <NavBar />
-            <Routes>       
-              <Route exact path='/' element={<ItemListContainer />}/>
-              <Route path='category/:categoryId' element={<ItemListContainer />}/>
-              <Route path='detail/:paramId' element={<ItemDetailContainer />} />
-              <Route path='cart' element={<Cart />}/>
-              <Route path='*' element={<NotFound />}/>
-            </Routes>
-          </BrowserRouter>
-      </CartProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>       
+          <Route exact path='/' element={<ItemListContainer />}/>
+          <Route path='category/:categoryId' element={<ItemListContainer />}/>
+          <Route path='detail/:paramId' element={<ItemDetailContainer />} />
+          <Route path='cart' element={<Cart />}/>
+          <Route path='*' element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
